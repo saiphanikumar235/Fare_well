bb7f015bdb2d464d57e2d0e354c45181702d0fd2
saiphanikumar235 <138872112+saiphanikumar235@users.noreply.github.com>
2023-12-21 18:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,9 @@ time.sleep(7)
 st.markdown("<h1 style='text-align: left; color: red;'>Sai's Farewell</h1>", unsafe_allow_html=True)
 st.toast("saiphanikumar2000@gmail.com", icon='📧')
 st.toast("https://www.linkedin.com/in/saiphani-kumar-b33431177/")
-st.write(
-    """As I prepare to embark on a new chapter in my career journey, I wanted to take a moment to express my deepest gratitude to each and every one of you.
-            
-            
-Working alongside such an incredible team has been an absolute privilege. The support, camaraderie, and shared experiences have made my time here truly memorable and rewarding.
-
-
-            
-I am profoundly grateful for the opportunities I've had to learn, grow, and contribute within this amazing company. The knowledge and experiences gained here will undoubtedly shape my future endeavors.\n
-            """,)
+st.write("""JDF has been my employer for the past two years. The experience was exceptional and involved a lot of exciting work and learning.\n\nAs I prepare to embark on a new chapter in my career journey, I wanted to take a moment to express my deepest gratitude to each and every one of you.\n\nIt has been an absolute privilege to work with such an incredible team.\n\nThe time I spent here has been truly memorable and rewarding because of the support, camaraderie, and shared experiences.\n\nThe opportunities I've had to learn, grow, and contribute within this amazing company have given me a great deal of gratitude.\n\nMy future endeavors will be shaped by the knowledge and experiences I have gained here.\n\nI am optimistic that we will come across each other soon..\n""", )
 time.sleep(5)
 st.toast("saiphanikumar2000@gmail.com", icon='📧')
 st.toast("https://www.linkedin.com/in/saiphani-kumar-b33431177/")
 c = st.container()
-c.text("https://www.linkedin.com/in/saiphani-kumar-b33431177/")
+c.text("Please feel free to connect with me over email-saiphanikumar2000@gmail.com \n\t\t\tor\nvia linkedin - https://www.linkedin.com/in/saiphani-kumar-b33431177/")
